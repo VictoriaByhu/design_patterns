4dc1adb5d66af677a3c409a4be13dc6be4be0524
Byhu Victoria <vikabygu@gmail.com>
2026-03-10 22:35:01 +0200

    Abstract factory pattern implemented

--- lab_1/src/creational/abstract_factory/GameArea.java
+++ lab_1/src/creational/abstract_factory/GameArea.java
@@ -3,14 +3,17 @@ package creational.abstract_factory;
 public class GameArea {
     private Monster monster;
     private Trap trap;
+    private TreasureChest chest;
 
     public GameArea(LevelFactory factory) {
         this.monster = factory.createMonster();
         this.trap = factory.createTrap();
+        this.chest = factory.createChest();
     }
 
     public void play() {
         monster.roar();
         trap.activate();
+        chest.open();
     }
 }
